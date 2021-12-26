633c7a19596080987b8831c057a72b5fb2a8b491
EXA-Hub <vip.mimo2015@gmail.com>
2021-12-27 01:02:54 +0200

    jj

--- React/src/components/MainPage.jsx
+++ React/src/components/MainPage.jsx
@@ -1,7 +1,21 @@
 import React, { Component } from "react";
+import { Dropdown } from "react-bootstrap";
 
 export default class MainPage extends Component {
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            جرب شيئا آخر
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item href="#">أرسل</Dropdown.Item>
+            <Dropdown.Item href="#">أعجبني</Dropdown.Item>
+            <Dropdown.Item href="#">لأ</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    );
   }
 }
