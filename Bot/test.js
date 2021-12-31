2f3fca7854b82fa6e146ca16b46349b949610db5
EXA-Hub <vip.mimo2015@gmail.com>
2021-12-31 21:11:00 +0200

    تصحيح مشاكل الأوامر لا تستجيب ✅

--- Bot/test.js
+++ Bot/test.js
@@ -1,25 +1,16 @@
-/*
 require("dotenv").config();
 
-const token = "",
-  id = "";
+// const token = "",
+//   id = "";
 const { token, id } = require("./data/config").bot;
-
-const fs = require("fs");
-const { Routes } = require("discord-api-types/v9");
 const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
 const rest = new REST({ version: "9" }).setToken(token);
 
 (async () => {
   const cmds = await rest.get(Routes.applicationCommands(id));
-  console.log(JSON.stringify(cmds, 2, 2));
-  fs.writeFile("./data/json.json", JSON.stringify(cmds), (e) => {
-    console.log(e);
-  });
-})();
-
-cmds.forEach((cmd) => {
-  (async () => {
+  console.log(cmds);
+  cmds.forEach(async (cmd) => {
     try {
       console.log("Started refreshing application (/) commands.");
 
@@ -29,36 +20,5 @@ cmds.forEach((cmd) => {
     } catch (error) {
       console.error(error);
     }
-  })();
-});
-
-(async () => {
-  console.log("Started refreshing application (/) commands.");
-
-  await rest.put(Routes.applicationCommands(id), {
-    body: require("./data/json.json"),
   });
-
-  console.log("Successfully deleted application (/) commands.");
 })();
-
-*/
-
-// filter:data
-
-/*
-const data = [
-  { type: 1, name: "213" },
-  { type: 2, name: "fsdffsd" },
-  { type: 1, name: "sdffd" },
-  { type: 2, name: "sdfs" },
-  { type: 1, name: "fd42423s" },
-];
-
-console.log(data);
-
-const filter = (x) => x.type === 1;
-const newData = data.filter(filter);
-
-console.log(newData);
-*/
