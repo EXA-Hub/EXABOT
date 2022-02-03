191f17a2c44bb83c58cb5d16256479360b2461a0
EXA-Hub <vip.mimo2015@gmail.com>
2022-02-04 00:03:11 +0200

    زراعة أخطاء

--- Bot/events/anticrash.js
+++ Bot/events/anticrash.js
@@ -11,47 +11,39 @@ module.exports = (client, instance) => {
   console.log("[antiCrash] :: Working Fine");
   const channel = client.channels.cache.get(errorsID);
   process
-    .on("unhandledRejection", async (reason, p) => {
+    .on("unhandledRejection", async (reason) => {
       console.log(" [antiCrash] :: Unhandled Rejection/Catch");
-      console.log(reason, p);
+      console.log(reason);
       channel.send({
-        content: `[antiCrash] :: Unhandled Rejection/Catch \n\`\`\`${
-          (reason, p.finally.toString())
-        }\`\`\``,
+        content: `[antiCrash] :: Unhandled Rejection/Catch \n\`\`\`${reason}\`\`\``,
       });
     })
     .on("uncaughtException", async (err, origin) => {
       console.log(" [antiCrash] :: Uncaught Exception/Catch");
       console.log(err, origin);
       channel.send({
-        content: `[antiCrash] :: Uncaught Exception/Catch \n\`\`\`${
-          (err, origin.toString())
-        }\`\`\``,
+        content: `[antiCrash] :: Uncaught Exception/Catch \n\`\`\`${err.name}: ${err.message}\`\`\``,
       });
     })
     .on("uncaughtExceptionMonitor", async (err, origin) => {
       console.log(" [antiCrash] :: Uncaught Exception/Catch (MONITOR)");
       console.log(err, origin);
       channel.send({
-        content: `[antiCrash] :: Uncaught Exception/Catch (MONITOR) \n\`\`\`${
-          (err, origin.toString())
-        }\`\`\``,
+        content: `[antiCrash] :: Uncaught Exception/Catch (MONITOR) \n\`\`\`${err.name}: ${err.message}\`\`\``,
       });
     })
     .on("multipleResolves", async (type, promise, reason) => {
       console.log(" [antiCrash] :: Multiple Resolves");
       console.log(type, promise, reason);
       channel.send({
-        content: `[antiCrash] :: Multiple Resolves \n\`\`\`${
-          (type.toString(), promise.finally.toString(), reason)
-        }\`\`\``,
+        content: `[antiCrash] :: Multiple Resolves \n\`\`\`${reason}\`\`\``,
       });
     })
     .on("warning", async (warning) => {
       console.log(" [antiCrash] :: Warning");
       console.log(warning);
       channel.send({
-        content: `[antiCrash] :: Warning \n\`\`\`${warning}\`\`\``,
+        content: `[antiCrash] :: Warning \n\`\`\`${warning.name}: ${warning.message}\`\`\``,
       });
     });
 };
