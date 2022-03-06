87bc03067e4dad5eb96ef528eacda8a6a26b4013
EXA-Hub <vip.mimo2015@gmail.com>
2022-03-06 22:14:33 +0200

    تم التصحيح

--- React/src/components/MainPage/MainPage.jsx
+++ React/src/components/MainPage/MainPage.jsx
@@ -395,7 +395,7 @@ export default function MainPage(props) {
                 </div>
                 <Tab.Container
                   id="left-tabs-example"
-                  defaultActiveKey="data"
+                  defaultActiveKey="Data"
                   key="TabContainer"
                 >
                   <Row>
